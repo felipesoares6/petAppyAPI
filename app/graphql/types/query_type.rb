712139be7766d68d5e4d512714d3bf9f28ada97b
Felipe Soares <felipeluizsoares@gmail.com>
2018-07-06 01:00:43 -0300

    Add usersAll query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,7 @@
-class Types::QueryType < Types::BaseObject
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
+Types::QueryType = GraphQL::ObjectType.define do
+  name 'Query'
 
-  # TODO: remove me
-  field :test_field, String, null: false,
-    description: "An example field added by the generator"
-  def test_field
-    "Hello World!"
+  field :allUsers, !types[Types::UserType] do
+    resolve ->(obj, args, ctx) { User.all }
   end
 end

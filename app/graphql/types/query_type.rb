1523ec7d6ad12fce0b9f09365d3645ad13e45713
Felipe Soares <felipeluizsoares@gmail.com>
2018-07-09 18:42:05 -0300

    Add user query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,4 +4,9 @@ Types::QueryType = GraphQL::ObjectType.define do
   field :users, !types[Types::UserType] do
     resolve ->(_obj, _args, _ctx) { User.all }
   end
+
+  field :user, types[Types::UserType] do
+    argument :id, !types.ID
+    resolve ->(_obj, args, _ctx) { User.where(id: args[:id]) }
+  end
 end
